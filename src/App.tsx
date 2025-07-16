216f538b64d5abb2e9f413debc9bdcc9d7825730
Ícarosun <icaropereirasun@gmail.com>
2025-07-16 16:29:14 -0400

    feature(add): queryClient

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,9 @@ import { TooltipProvider } from "./components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
-import NotFound from "./pages/NotFound";
+import NotFound from "./pages/NotFound.tsx";
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
