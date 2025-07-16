d20d4e77bae3d66cf10594b3b44152c875d10b5d
Ícarosun <icaropereirasun@gmail.com>
2025-07-16 18:35:28 -0400

    fix: currentView

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -14,7 +14,7 @@ interface Content {
 }
 
 const Index = () => {
-  const [cureentView, setCurrentView] = useState<'dashboard' | 'display' | 'preview'>('dashboard');
+  const [currentView, setCurrentView] = useState<'dashboard' | 'display' | 'preview'>('dashboard');
   const [isPlaying, setIsPlaying] = useState(false);
   const [content, setContent] = useState<Content[]>([
      {
@@ -33,7 +33,7 @@ const Index = () => {
       id: '2',
       type: 'image',
       title: 'Tecnologia Avançada',
-      content: signageBg1,
+      content: "https://imgs.search.brave.com/_6izdbSVpHbwqbawMBquENAqLf6WtvwCZmTG8AhitpY/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9jZG4t/ZnJvbnQuZnJlZXBp/ay5jb20vaG9tZS9h/bm9uLXJ2bXAvY3Jl/YXRpdmUtc3VpdGUv/cGhvdG9ncmFwaHkv/Y2hhbmdlLWxvY2F0/aW9uLndlYnA",
       duration: 8000,
       interactive: true,
       cta: {
@@ -85,6 +85,11 @@ const Index = () => {
           <div className="">
             <div className="">
               <h1 className="">Digital Signage</h1>
+              <div className="">
+                <button onClick={() => setCurrentView("display")}>
+                  Teste
+                </button>
+              </div>
             </div>
           </div>
         </div>
